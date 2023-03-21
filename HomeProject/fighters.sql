f265fe029c42611e5e1b6cac8b7fd1165e39f475
Driftingboat <>
2023-03-21 12:53:31 +0900

    sd

--- HomeProject/fighters.sql
+++ HomeProject/fighters.sql
@@ -1,5 +1,10 @@
 select * from player_login ;
 
+select * from player_login;
+select * from player_login;
+select * from player_login;
+select * from player_login;
+select * from player_login;
 
 insert into player_login values('test','test');
 commit;
